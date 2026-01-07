cca1d51b3b44708757f95a93a91c9fc897829cb7
Vasavireddy956 <vasavimamidi61@gmail.com>
2026-01-07 20:56:37 +0000

    Update Stack.cs

--- Project1-Expression-Evaluation/Project1-Expression-Evaluation/Stack.cs
+++ Project1-Expression-Evaluation/Project1-Expression-Evaluation/Stack.cs
@@ -1,1 +1,32 @@
+public class Stack<T>
+{
+    private T[] data;
+    private int top;
+
+    public Stack(int size)
+    {
+        data = new T[size];
+        top = -1;
+    }
+
+    public void Push(T item)
+    {
+        data[++top] = item;
+    }
+
+    public T Pop()
+    {
+        return data[top--];
+    }
+
+    public T Peek()
+    {
+        return data[top];
+    }
+
+    public bool IsEmpty()
+    {
+        return top == -1;
+    }
+}
 
